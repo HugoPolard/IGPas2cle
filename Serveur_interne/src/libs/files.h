eb7314eef60920735c3aa04a5ff4eac6a7738dbb
DEFOY <gregoire.defoy@ig2i.centralelille.fr>
2020-04-14 16:20:08 +0200

    erreur sur le dernier commit

---
+++ Serveur_interne/src/libs/files.h
@@ -0,0 +1,15 @@
+//
+// Created by gregoire on 08/04/2020.
+//
+
+#ifndef SERVEUR_INTERNE_FILES_H
+#define SERVEUR_INTERNE_FILES_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int get_lines_in_file(char *filename);
+char *readLastLine(char *filename);
+
+#endif //SERVEUR_INTERNE_FILES_H
---
+++ Serveur_interne/src/libs/split.h
@@ -0,0 +1,17 @@
+//
+// Created by gregoire on 09/04/2020.
+//
+
+#ifndef SERVEUR_INTERNE_SPLIT_H
+#define SERVEUR_INTERNE_SPLIT_H
+
+/**
+ *
+ * @param string la chaine à découper
+ * @param delimiter la chaine qui délimite les parties de la chaine
+ * @param num le numéro du champ
+ * @return
+ */
+char *split(char *string, char *delimiter, int num);
+
+#endif //SERVEUR_INTERNE_SPLIT_H
